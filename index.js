352a383992a37d28f74c31fa9ba4f22555ae3ed7
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-20 00:16:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users',{
+         method: 'POST',
+         headers: {
+            'content-Type' : 'application/json',
+            Accept: 'application/json'
+         },
+         body: JSON.stringify({name, email})
+      })
+      
+      .then(response => response.json())
+      .then(dataGot => document.body.innerHTML = dataGot['id'])
+      
+      .catch((error) => {
+          document.body.innerHTML = error.message
+let message='Unauthorized Access'
+          
+      })
+      
+    }
